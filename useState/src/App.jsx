ea9f8a8ae91ed79752d9e20d4ac75516ac168a32
Evakshu Pratap Singh <evakshupratapsingh@gmail.com>
2026-06-23 14:34:15 +0530

    useState commit

---
+++ useState/src/App.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [num, setNum] = useState(10)
+  const btnClicked = () => {
+  };
+
+  return (
+    <div>
+      <h1>{num}</h1>
+      <button onClick={btnClicked}>Click</button>
+    </div>
+  );
+};
+
+export default App;
+
+// import React,{ useState } from "react"
+// const App = () => {
+// //const [num, setNum] = useState(10)
+// // function onincrease() {
+// //setNum(num + 1)
+// // }
+// //function decrease() {
+// //setNum(num - 1)
+
+// //}
+// //return (
+// // <div>
+// //  <h1>{num}</h1>
+// //  <button onClick={onincrease}>Increase</button>
+// //  <button onClick={decrease}>Decrease</button>
+// // </div>
+// //)
+// //}
+
+// //export default App
+
+// //import React, { useState } from 'react';
+// //const App = () => {
+// // const [first, setFirst] = useState(1);
+// // function onChange() {
+// //  setFirst(20);
+// //}
+
+// // return (
+// //  <div>
+// //   <h1>Value of first is {first}</h1>
+// //   <button onClick={onChange}>Click</button>
+// //</div>
+// // );
+// //};
+
+// //export default App;
